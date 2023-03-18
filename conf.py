5a2c3b85ca3a612861a0ac598c5d5729565666ac
Václav Šmilauer <eu@doxos.eu>
2023-03-18 20:43:37 +0100

    HTML: show full TOC in the sidebar

--- conf.py
+++ conf.py
@@ -43,6 +43,7 @@ except subprocess.CalledProcessError:
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinxcontrib.fulltoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
